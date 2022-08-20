5c268eba3a76f5814f86f1cb5fb2c97111319230
Lucas <48027211+LucasGivord@users.noreply.github.com>
2022-08-20 21:02:58 +0200

    Add mouse click system

---
+++ Assets/Scripts/modules/Controllers/InputControllerManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputControllerManager : MonoBehaviour
+{
+  #region VARIABLES
+
+  private static InputControllerManager Instance;
+
+  #endregion
+
+  #region UNITY_METHODS
+
+  private void Awake()
+  {
+    if (Instance == null)
+    {
+      Instance = this;
+    }
+  }
+
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Mouse0))
+    {
+      RaycastHit hit;
+      Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+      if (Physics.Raycast(ray, out hit))
+      {
+        Transform objectHit = hit.transform;
+        if (objectHit.gameObject.transform.parent.gameObject.transform.parent.name == "GridManager")
+        {
+          TriggerGrid(objectHit.gameObject.transform.parent.gameObject.name);
+        }
+        else
+        {
+          // Detect other think...
+          Debug.Log("object hit name : " + objectHit.gameObject.name);
+        }
+      }
+    }
+  }
+  #endregion
+
+  #region METHODS
+  public static InputControllerManager GetInstance()
+  {
+    if (Instance == null)
+    {
+      Instance = new InputControllerManager();
+    }
+    return Instance;
+  }
+
+  private void TriggerGrid(string gridName)
+  {
+    int index;
+    int.TryParse(gridName, out index);
+
+    GridManager.GetInstance().UpdateGridAt(index);
+  }
+  #endregion
+}
---
+++ Assets/Scripts/modules/Grid/GridManager.cs
@@ -0,0 +1,154 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+/**
+ * Singleton class used to manage Grid for external system. Contains only
+ * an instance and methods to access of a Grid.
+ */
+public class GridManager : MonoBehaviour
+{
+  #region VARIABLES
+
+  /**
+   * Basic instance used later for any action on the grid.
+   */
+  private static GridManager Instance;
+
+  /**
+   * Internal stored grid.
+   */
+  private Grid grid = new Grid();
+
+  /**
+   * Dimensions of our grid.
+   */
+  public uint cols = 5;
+  public uint rows = 5;
+
+  /**
+   * Represent the offset needed betwwen each tile.
+   */
+  public float offsetTile = 6;
+
+  public List<GameObject> prefabs;
+
+  #endregion
+
+  #region UNITY_METHODS
+
+  /**
+   * Naive approach to generate our Singleton, we should switch to an
+   * "lazzy loading" approach later.
+   */
+  void Awake()
+  {
+    if (Instance == null)
+    {
+      Instance = this;
+      if (GenerateGrid(rows, cols))
+      {
+        InstantiateGridPrefab();
+      }
+
+      Print();
+    }
+    else
+    {
+      Destroy(this);
+    }
+  }
+
+  #endregion
+
+  #region METHODS
+
+  public static GridManager GetInstance()
+  {
+    if (Instance == null)
+    {
+      Instance = new GridManager();
+    }
+
+    return Instance;
+  }
+
+  /**
+   * Generate a new Grid only if there is not grid already created.
+   */
+  public bool GenerateGrid(uint rows = 10, uint cols = 10)
+  {
+    return grid.Create(rows, cols);
+  }
+
+  /**
+   * Instantiate all prefab object from the grid.
+   * Need to call GenerateGrid() before this method.
+   */
+  public bool InstantiateGridPrefab()
+  {
+    if (grid == null)
+    {
+      Debug.LogWarning("Need to call Generate Grid before this method.");
+      return false;
+    }
+
+    // Check prefabs initialization
+    if (prefabs.Count == 0)
+    {
+      Debug.LogWarning("prefabs lists need to have at least the tile default prefab.");
+      return false;
+    }
+
+    for (uint i = 0; i < rows; i++)
+    {
+      for (uint j = 0; j < cols; j++)
+      {
+        Tile tile = new Tile();
+        grid.At(tile, i, j);
+        switch (tile.state)
+        {
+          case Tile.State.Empty:
+            break;
+          case Tile.State.None:
+            tile.state = Tile.State.Empty;
+            Vector3 offsetPos = new Vector3(i * offsetTile, 0, j * offsetTile);
+            var prefabParent = new GameObject();
+            prefabParent.transform.parent = gameObject.transform;
+            prefabParent.transform.position = offsetPos;
+            prefabParent.name = (i * rows + j).ToString();
+            Instantiate(prefabs[0], prefabParent.transform);
+            break;
+          default:
+            break;
+        }
+      }
+    }
+
+    return true;
+  }
+
+  public void UpdateGridAt(int index)
+  {
+    Debug.Log("index : " + index);
+    Debug.Log("UpdateGridAt : " + gameObject.transform.GetChild(index).gameObject.name);
+    var tile = gameObject.transform.GetChild(index).gameObject;
+    if (tile)
+    {
+      var tileToDestroy = tile.transform.GetChild(0).gameObject;
+      Destroy(tileToDestroy);
+      Instantiate(prefabs[1], tile.transform.position, Quaternion.identity, tile.transform);
+    }
+  }
+
+  /**
+   * Convenient method to print all informations.
+   */
+  public void Print(bool withDetails = false)
+  {
+    Debug.Log(Instance);
+    grid.Print();
+  }
+  #endregion
+}
